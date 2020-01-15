4273507efe87c974c5e2ce550d524d2bf6cf8672
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2020-01-14 22:36:17 -0700

    google-cloud-logging added to code

--- tracker.py
+++ tracker.py
@@ -3,25 +3,42 @@ import time
 import sys
 from datetime import datetime
 from flask import escape
+from google.cloud import logging
 
 
 def stock_tracker(request):
-    def get_price(stock_name):
+    logging_client = logging.Client()
+    log_name = 'my-log'
+    logger = logging_client.logger(log_name)
+    #logger.log_text(text)
+    def get_price(stock_name, lc=logging_client):
+        log_name = 'my-get_price'
+        logger = lc.logger(log_name)
         r = si.get_quote_table(stock_name, dict_result=False)
-        print("stock_tracker stock_tracker {}".format(stock_name))
+        message1 = log_name+" stock_name {}".format(stock_name)
+        logger.log_text(message1)
+        print(message1)
         stockPrice = r.loc[15]['value']
-        print("stock_tracker stock_tracker {}".format(stockPrice))
+        message2 = log_name+" stockPrice {}".format(stockPrice)
+        logger.log_text(message2)
+        print(message2)
         return stockPrice
     request_json = request.get_json(silent=True)
     request_args = request.args
     if request_json and 'stock_name' in request_json:
         stock_name = request_json['stock_name']
-        print("stock_tracker stock_name request_json {}".format(stock_name))
+        msgi = "stock_tracker stock_name request_json {}".format(stock_name)
+        logger.log_text(msgi)
+        print(msgi)
     elif request_args and 'stock_name' in request_args:
         stock_name = request_args['stock_name']
-        print("stock_tracker stock_name request_args {}".format(stock_name))
+        msgi = "stock_tracker stock_name request_args {}".format(stock_name)
+        logger.log_text(msgi)
+        print(msgi)
     else:
         stock_name = 'dis'
     stock_price = get_price(stock_name)
-    print("stock_tracker stock_name stock_price {}".format(stock_price))
+    msgf = "stock_tracker stock_name stock_price {}".format(stock_price)
+    logger.log_text(msgf)
+    print(msgf)
     return 'Hello {0} price is {1}!'.format(escape(stock_name), stock_price)
